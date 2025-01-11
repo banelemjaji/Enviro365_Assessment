2234352e21134fb0b6948c6745d25666a149a80a
banelemjaji <banelemjaji@gmail.com>
2025-01-11 23:18:50 +0200

    first commit

---
+++ src/test/java/com/enviro/assessment/grad001/banelemjaji/WasteSortingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.banelemjaji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteSortingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
